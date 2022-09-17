2b6989ba60feb9dba095a5499e491f61c705a4cb
testler <joshgarst@me.com>
2022-09-17 13:19:03 -0500

    postgres intergration

--- src/main/java/AllThingsByAV/backend/controllers/OrderController.java
+++ src/main/java/AllThingsByAV/backend/controllers/OrderController.java
@@ -1,14 +1,14 @@
 package AllThingsByAV.backend.controllers;
 
-//import AllThingsByAV.backend.repository.MainRepository;
+import AllThingsByAV.backend.repository.MainRepository;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/order")
 public class OrderController {
-//    private final MainRepository mainRepository;
-//
-//    public OrderController(MainRepository mainRepository) {
-//        this.mainRepository = mainRepository;}
+    private final MainRepository mainRepository;
+
+    public OrderController(MainRepository mainRepository) {
+        this.mainRepository = mainRepository;}
 }
--- src/main/java/AllThingsByAV/backend/controllers/UserController.java
+++ src/main/java/AllThingsByAV/backend/controllers/UserController.java
@@ -1,17 +1,17 @@
 package AllThingsByAV.backend.controllers;
 
-//import AllThingsByAV.backend.repository.MainRepository;
+import AllThingsByAV.backend.repository.MainRepository;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/user")
 public class UserController {
-//    private final MainRepository mainRepository;
-//
-//
-//
-//    public UserController(MainRepository mainRepository) {
-//        this.mainRepository = mainRepository;
-//    }
+    private final MainRepository mainRepository;
+
+
+
+    public UserController(MainRepository mainRepository) {
+        this.mainRepository = mainRepository;
+    }
 }
